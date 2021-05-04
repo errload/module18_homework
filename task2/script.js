f257d1486e78c6aaa3c187358224f94598ab9e11
errload <errload@mail.ru>
2021-05-04 21:56:20 +0300

    finish task 3

--- task2/script.js
+++ task2/script.js
@@ -1,4 +1,9 @@
-let btn = document.querySelector('.btn');
+/*
+    Сверстайте кнопку, которая будет содержать в себе icon_01 (как в примере в последнем видео). 
+    При клике на кнопку иконка должна меняться на icon_02. Повторный клик меняет иконку обратно.
+*/
+
+const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('btn--magic');
---
+++ task3/script.js
@@ -0,0 +1,48 @@
+/*
+    Сверстайте кнопку, клик на которую будет выводить на экран следующие данные:
+        - Размеры экрана пользователя (ширина и высота).
+        - Координаты местонахождения пользователя. Если пользователь отказался дать доступ 
+        к местоположению или данная функция недоступна в бразуере, вывести вместо координат 
+        сообщение «Информация о местоположении недоступна».
+*/
+
+const btn = document.querySelector('.btn');
+const sizeWindow = document.querySelector('.size__window');
+const geoPosition = document.querySelector('.geo__position');
+
+// функция определения размеров окна
+function getSizeWindow() {
+    const width = window.screen.width;
+    const height = window.screen.height;
+
+    // выводим размеры экрана
+    sizeWindow.textContent = `Ширина экрана: ${width} px, высота экрана: ${height} px`;
+}
+
+// функция ошибки определения геопозиции
+function geoError() {
+    geoPosition.textContent = 'Информация о местоположении недоступна.';
+}
+
+// функция успешного определения геопозиции
+function geoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    
+    // вывод геопозиции на экран
+    geoPosition.textContent = `Широта: ${latitude}, долгота: ${longitude}`;
+}
+
+// функция нажатия кнопки
+btn.addEventListener('click', () => {
+    // выводим размеры экрана
+    getSizeWindow();
+
+    // определяем геолокацию
+    if (!navigator.geolocation) {
+        geoError();
+    } else {
+        geoPosition.textContent = 'Определяем координаты...';
+        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    }
+});
